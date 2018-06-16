85701587dda5ddd8eee4bc2bb55a47b50a2f1299
Nikita Sychev <root@nsychev.ru>
2018-06-16 05:45:02 +0300

    add morersa task

---
+++ morersa/private/gen.py
@@ -0,0 +1,16 @@
+p = 0xFADEB267B75E4C39 # openssl prime -generate -bits 64 -hex
+q = 0xC80814084DDD7751
+n = p * q
+f = (p - 1) * (q - 1)
+
+e = 0x10001
+d = gmpy2.invert(e, f)
+
+flag = "too_small_rsa"
+c = 0x746f6f5f736d616c6c5f727361 # codecs.encode(flag, hex)
+ce = pow(c, e, n)
+cd = pow(ce, d, n)
+
+assert(cd == c)
+
+print(n, e, ce)
